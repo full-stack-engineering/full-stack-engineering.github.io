71f2cc6b1f266ada578a7d8f9fed36204bebd688
Mateusz Chrzonstowski <mateusz@brightcube.eu>
2020-05-18 00:24:42 +0200

    Add initial alert

---
+++ src/alert.ts
@@ -0,0 +1,32 @@
+import {getApiCourses} from './service';
+
+export default function register() {
+    customElements.define('m3-alert', M3Alert);
+}
+
+class M3Alert extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    async connectedCallback() {
+        const {coupon, deadline} = await getApiCourses();
+        const localDeadline = new Date(deadline);
+        const deadlineString = new Intl.DateTimeFormat('pl', {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        }).format(localDeadline);
+        if (new Date() < localDeadline) {
+            this.innerHTML = `
+                <div class="e-alert warning">
+                  <i class="fas fa-exclamation-circle e-icon"></i> 
+                  Kursy po <strong>48,99</strong> z kuponem "<strong>${coupon}</strong>" (linki poniżej)!
+                </div>
+            `;
+        }
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import initCards from "./card";
+import initCards from './card';
+import initAlert from './alert';
 // import initTiles from "./tile";
 // import initLayout from "./layout";
 
 initCards();
+initAlert();
 // initTiles();
 // initLayout();
